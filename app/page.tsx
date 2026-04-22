856e5be11283bccec2a8c1bc7b84e482e657f60b
JosephinedeAz <josephine@deazstudio.com>
2026-04-22 13:20:12 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ function Navbar() {
               href="#contact"
               className="text-[22px] text-black text-center w-[149px]"
             >
-              Contact
+              Contact 56
             </Link>
           </div>
         </div>
